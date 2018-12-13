ea1d267d71178b2bf09f99e027c69c9ba5cdd66f
Dylan Thomson <djthomson88@gmail.com>
2018-12-13 14:01:44 -0500

    Testing search by shelter

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,18 @@
 // Testing basic API call, check console for results
 $.ajax({
-    url: "http://api.petfinder.com/pet.find?key=7dc1511d0faaadd24a44d60d637a14d8&location=44060&format=json" + "&callback=?",
+    url: "http://api.petfinder.com/pet.find?key=7dc1511d0faaadd24a44d60d637a14d8&location=44060&output=full&format=json" + "&callback=?",
+    type: "GET",
+    dataType: "json"
+}).then((response) => {
+    console.log(response);
+}).fail((error) => {
+    console.log("Failure to get API data", error);
+})
+
+
+
+$.ajax({
+    url: "http://api.petfinder.com/shelter.get?key=7dc1511d0faaadd24a44d60d637a14d8&id=OH500&format=json" + "&callback=?",
     type: "GET",
     dataType: "json"
 }).then((response) => {
