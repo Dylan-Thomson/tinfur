ca00a0fbe06ac5451ef0d2f3b4c283f0c3076628
Stacy Acker <stacyacker@Stacys-MacBook-Pro.local>
2018-12-19 10:26:05 -0500

    change logo and jumbotron colors

---
+++ tinfur.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+// <script src="https://www.gstatic.com/firebasejs/5.7.0/firebase.js"></script>
+// <script>
+//   // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDIqW4bR9m_GYR06Fd_mdpQ2_CgNdSyIAg",
+    authDomain: "tin-fur.firebaseapp.com",
+    databaseURL: "https://tin-fur.firebaseio.com",
+    projectId: "tin-fur",
+    storageBucket: "tin-fur.appspot.com",
+    messagingSenderId: "880903844640"
+  };
+
+  firebase.initializeApp(config);
+// </script>
+// }
+
+  var database = firebase.database();
+
+  
+
+// }
+
+// firebase.auth().createUserWithuserNameAndPassword(uname, psw).catch(function(error) {
+//     console.log(error.code);
+//     console.log(error.message);
+//   });
+
+
+
